2851c77b014619e7ab38a6bcf42f3a17516ec559
Amir Emon <prince.vince.tech@gmail.com>
2022-11-24 08:34:15 -0500

    added adapter pswh

---
+++ powershell/adapters.ps1
@@ -0,0 +1,5 @@
+﻿#create script for network adapter report using Get-CimInstance Win32_NetworkAdapterConfiguration class
+#look for enabled adapter only
+Get-CimInstance Win32_NetworkAdapterConfiguration -filter "IPEnabled = 'True'" |
+#create an object and have it display Description, Index, IPAddress, SubnetMask, DNSDomain and DNS Server in table
+Select-Object Description, Index, IPAddress, SubnetMask, DNSDomain, DNSServerSearchOrder | Format-Table
