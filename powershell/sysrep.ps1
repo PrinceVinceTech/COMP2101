919fd90dac38e1695ec1a57528979601187450a2
Amir Emon <prince.vince.tech@gmail.com>
2022-12-15 09:10:51 -0500

    Update 005 sysprep

--- powershell/sysrep.ps1
+++ powershell/sysrep.ps1
@@ -12,17 +12,19 @@ $hardWdescription | Format-Table *
 "----------------"
 #create information about operating system name, version number (win32_operatingsystem)
 $operatingSystem = Get-CIMInstance Win32_OperatingSystem
-$operatingSystem | Format-Table *
+#user Format-Table to output the information with only the System Name and Version
+$operatingSystem | Format-Table SystemName, Version
 
 "CPU Information"
 "---------------"
 #create information about Processor description with speed, number of cores, and sizes of L1, L2, and L3 caches if they are present (win32_processor)
 $cpuInfo = Get-CIMInstance Win32_Processor
-#user Format-Table to output the information in a table
-$cpuInfo | Format-Table *
+#user Format-Table to output the information in a table and only include L1, L2, and L3 caches if they are present
+$cpuInfo | Format-Table Name, Description, MaxClockSpeed, NumberOfCores, L1CacheSize -eq, L2CacheSize, L3CacheSize
 
 #create information summary of RAM installed with vendor, description, size, and bank and slot for each RAM module
 $ramInfo = Get-CIMInstance Win32_PhysicalMemory
+$ramInfo | Format-Table Vendor, Description, Capacity, BankLabel, DeviceLocator
 
 #create a summary of physical disk with their vendor, model, size, and space usage (size, free space, and percentage free) and format on a signle table with one local disk per output line
 $diskInfo = Get-CIMInstance CIM_logicaldisk
@@ -41,7 +43,7 @@ foreach ($disk in $diskInfo) {
     }
 }
 #use adapters.ps1 to get the network adapter information
-$networkInfo = .\adapters.ps1
+$networkInfo = adapters.ps1
 $networkInfo
 
 #create a report for videocard vendor, description, and memory size
